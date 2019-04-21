36e5353312970c178572a2502f377eb94ebdf2b3
Igor Camões <itscamoes@gmail.com>
2019-04-21 17:58:07 -0300

    Terminar menu responsivo no tablet

--- script.js
+++ script.js
@@ -1,13 +1,37 @@
-let nomePatos = document.querySelector(".patos");
-let btnPatos = document.querySelector(".botoesPato");
+let patoLogo = document.querySelector("#navMobile a img");
+let nav = document.querySelector("nav");
+let btnHmb = document.querySelector("#divHmb");
+let spanTopo = document.querySelector(".spanTopo");
+let spanMeio = document.querySelector(".spanMeio");
+let spanBase = document.querySelector(".spanBase");
 
 
-btnPatos.onclick = patoHover;
 
-function patoHover(){
-    nomePatos.style.color = "black";
-    btnPatos.style.backgroundColor = "white";
-    nomePatos.style.cursor = "pointer";
+
+function animacaoHmb(){
+    spanTopo.classList.toggle("topoXis");
+    spanMeio.classList.toggle("meioSome");
+    spanBase.classList.toggle("baseXis");
+    patoLogo.classList.toggle("quack");
+    nav.classList.toggle("desceDai");
 }
 
+// function menuDeslizar(){
+//     if(body.style.gridTemplateRows = "10vh 75vh 15vh 120vh 20vh"){
+//         body.style.gridTemplateRows = "10vh 30vh 75vh 15vh 120vh 20vh";
+//     }
+//     else if(body.style.gridTemplateRows = "10vh 30vh 75vh 15vh 120vh 20vh"){
+//     body.style.gridTemplateRows = "10vh 75vh 15vh 120vh 20vh";
+//     }
 
+//     if(body.style.gridTemplateAreas = '"nv" "hd" "sc" "art" "ftr"'){
+//         body.style.gridTemplateAreas = '"nv" "nv2" "hd" "sc" "art" "ftr"';
+//     }
+//     else if(body.style.gridTemplateAreas = '"nv" "nv2" "hd" "sc" "art" "ftr"'){
+//         body.style.gridTemplateAreas = '"nv" "hd" "sc" "art" "ftr"';
+//     }
+// }
+
+btnHmb.onclick = animacaoHmb;
+
+// btnHmb.onclick = menuDeslizar;
